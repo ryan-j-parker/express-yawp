d04c13e7f90d562787ea9cd760cb07bb8ed49a6b
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-28 15:04:14 -0800

    getAll test passing

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -3,18 +3,52 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('user routes', () => {
+describe('restaurants routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
   });
 
   it('GET /api/v1/restaurants should retrieve a list of restaurants', async () => {
     const res = await request(app).get('/api/v1/restaurants');
-    expect(res.status).toEqual(200);
-    expect(res.body).toMatchInlineSnapshot();
-  });
-
-  afterAll(() => {
-    pool.end();
+    expect(res.status).toBe(200);
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "cost": 1,
+          "cuisine": "American",
+          "id": "1",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+          "name": "Pip's Original",
+          "website": "http://www.PipsOriginal.com",
+        },
+        Object {
+          "cost": 3,
+          "cuisine": "Italian",
+          "id": "2",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+          "name": "Mucca Osteria",
+          "website": "http://www.muccaosteria.com",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "Mediterranean",
+          "id": "3",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+          "name": "Mediterranean Exploration Company",
+          "website": "http://www.mediterraneanexplorationcompany.com/",
+        },
+        Object {
+          "cost": 2,
+          "cuisine": "American",
+          "id": "4",
+          "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+          "name": "Salt & Straw",
+          "website": "https://saltandstraw.com/pages/nw-23",
+        },
+      ]
+    `);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS restaurants CASCADE;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews CASCADE;
 
-DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
